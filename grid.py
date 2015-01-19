44ccba0f9296cf1eb98fd5f55b7800fb61f42fa7
Dominik Tomicevic <dominik.tomicevic@gmail.com>
2015-01-19 12:16:02 +0100

    increased RAM cache size for SVM to train faster

--- grid.py
+++ grid.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 def grid_search(grid, inputs, targets, threads=cpu_count(), verbose=3):
-    model = GridSearchCV(svm.SVC(), grid, n_jobs=threads, verbose=verbose)
+    model = GridSearchCV(svm.SVC(cache_size=2000), grid, n_jobs=threads, verbose=verbose)
     model.fit(inputs, targets)
     return model.best_estimator_
 
